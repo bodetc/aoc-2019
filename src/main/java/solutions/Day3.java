6437440259b93fb532f2ae2bbe8cca58908206bf
Cédric Bodet <cedric.bodet@futurice.com>
2019-12-04 13:39:54 +0200

    Day 3 - Refactoring and simplifications

--- src/main/java/solutions/Day3.java
+++ src/main/java/solutions/Day3.java
@@ -16,21 +16,19 @@ public class Day3 {
     private enum Direction {
         R, L, U, D;
 
-        private static Direction fromString(String string) {
-            switch (string) {
-                case "R":
-                    return R;
-                case "L":
-                    return L;
-                case "U":
-                    return U;
-            }
-            throw new IllegalStateException("This point cannot be reached.");
+        private boolean isHorizontal() {
+            return this == R || this == L;
+        }
+
+        private boolean isVertical() {
+            return this == U || this == D;
         }
     }
 
     private static class Point {
         private final int x, y;
+
+        private static final Point ORIGIN = new Point(0, 0);
 
         private Point(int x, int y) {
             this.x = x;
@@ -42,95 +40,95 @@ public class Day3 {
         }
 
         private boolean equals(Point other) {
-            return x==other.x && y==other.y;
+            return x == other.x && y == other.y;
         }
 
-        private Point createPoint(PathSegment segment) {
-            switch (segment.direction) {
+        private Point addVector(Vector vector) {
+            switch (vector.direction) {
                 case R:
-                    return new Point(x + segment.distance, y);
+                    return new Point(x + vector.distance, y);
                 case L:
-                    return new Point(x - segment.distance, y);
+                    return new Point(x - vector.distance, y);
                 case U:
-                    return new Point(x, y + segment.distance);
+                    return new Point(x, y + vector.distance);
                 case D:
-                    return new Point(x, y - segment.distance);
+                    return new Point(x, y - vector.distance);
             }
             throw new IllegalStateException("This point cannot be reached.");
         }
     }
 
-    private static class PathSegment {
+    private static class Vector {
         private final Direction direction;
         private final int distance;
 
-        private PathSegment(Direction direction, int distance) {
-            this.direction = direction;
-            this.distance = distance;
+        private Vector(Vector other) {
+            this.direction = other.direction;
+            this.distance = other.distance;
         }
 
-        private PathSegment(String string) {
-            this.direction = Direction.valueOf(string.substring(0, 1));
-            this.distance = Integer.parseInt(string.substring(1));
+        private Vector(String vector) {
+            this.direction = Direction.valueOf(vector.substring(0, 1));
+            this.distance = Integer.parseInt(vector.substring(1));
+        }
+
+        boolean isHorizontal() {
+            return direction.isHorizontal();
+        }
+
+        boolean isVertical() {
+            return direction.isVertical();
+        }
+
+        int length() {
+            return distance;
         }
     }
 
-    private static class Segment {
+    private static class Segment extends Vector {
         private final Point start, end;
 
-        private Segment(Point start, Point end) {
+        private Segment(Point start, Vector vector) {
+            super(vector);
             this.start = start;
-            this.end = end;
+            this.end = start.addVector(this);
         }
 
         private Optional<Point> intersect(Segment other) {
-            double denom = (other.end.y - other.start.y) * (end.x - start.x) - (other.end.x - other.start.x) * (end.y - start.y);
-
-            if (denom == 0.) {
-                // Lines are parallel.
-                return Optional.empty();
-            }
-
-            double ua = ((other.end.x - other.start.x) * (start.y - other.start.y) - (other.end.y - other.start.y) * (start.x - other.start.x)) / denom;
-            double ub = ((end.x - start.x) * (start.y - other.start.y) - (end.y - start.y) * (start.x - other.start.x)) / denom;
-
-            if (ua >= 0. && ua <= 1. && ub >= 0. && ub <= 1.0f) {
-                // Get the intersection point.
-                return Optional.of(new Point((int) (start.x + ua * (end.x - start.x)), (int) (start.y + ua * (end.y - start.y))));
+            if (isHorizontal() && other.isVertical()
+                    && MathUtils.isBetween(start.x, end.x, other.start.x)
+                    && MathUtils.isBetween(other.start.y, other.end.y, start.y)) {
+                return Optional.of(new Point(other.start.x, start.y));
+            } else if (isVertical() && other.isHorizontal()
+                    && MathUtils.isBetween(other.start.x, other.end.x, start.x)
+                    && MathUtils.isBetween(start.y, end.y, other.start.y)) {
+                return Optional.of(new Point(start.x, other.start.y));
             } else {
-                // Intersection is outside bounds
                 return Optional.empty();
             }
-        }
-
-        private boolean isColinear(Point point) {
-            return (end.x - start.x) * (point.y - start.y) == (point.x - start.x) * (end.y - start.y);
         }
 
         private boolean contains(Point point) {
-            return isColinear(point) && MathUtils.isBetween(start.x, end.x, point.x)  && MathUtils.isBetween(start.y, end.y, point.y);
-        }
-
-        private int length() {
-            return start.taxiDistance(end);
+            return (isHorizontal() && start.y==point.y && MathUtils.isBetween(start.x, end.x, point.x))
+                   ||  (isVertical() && start.x==point.x && MathUtils.isBetween(start.y, end.y, point.y));
         }
     }
 
     private static class Line {
         private final List<Segment> segments = new ArrayList<>();
 
-        private Line(List<PathSegment> path) {
-            Point a = new Point(0, 0);
+        private Line(List<Vector> path) {
+            Point start = new Point(0, 0);
 
-            for (PathSegment segment : path) {
-                Point b = a.createPoint(segment);
-                segments.add(new Segment(a, b));
-                a = b;
+            for (Vector vector : path) {
+                Segment segment = new Segment(start, vector);
+                segments.add(segment);
+                start=segment.end;
             }
         }
 
         private Line(String path) {
-            this(COMMA_SEPARATOR.splitAsStream(path).map(PathSegment::new).collect(Collectors.toList()));
+            this(COMMA_SEPARATOR.splitAsStream(path).map(Vector::new).collect(Collectors.toList()));
         }
 
         private List<Point> intersect(Line other) {
@@ -138,17 +136,18 @@ public class Day3 {
                     .flatMap(s1 -> other.segments.stream().map(s1::intersect))
                     .filter(Optional::isPresent)
                     .map(Optional::get)
+                    .filter(Predicate.not(Point.ORIGIN::equals)) // Remove origin
                     .collect(Collectors.toList());
         }
 
         private int delay(Point point) {
             int delay = 0;
             for (Segment segment : segments) {
-                if(segment.contains(point)) {
-                    delay+=point.taxiDistance(segment.start);
+                if (segment.contains(point)) {
+                    delay += point.taxiDistance(segment.start);
                     return delay;
                 } else {
-                    delay+=segment.length();
+                    delay += segment.length();
                 }
             }
             return delay;
@@ -157,9 +156,7 @@ public class Day3 {
 
 
     private static int findIntersection(Line a, Line b, ToIntFunction<Point> distance) {
-        Point origin = new Point(0, 0);
         return a.intersect(b).stream() // Find intersections
-                .filter(Predicate.not(origin::equals)) // Remove origin
                 .mapToInt(distance) // Get distances
                 .min() // Get closest distance
                 .orElseThrow();
@@ -170,7 +167,7 @@ public class Day3 {
         return findIntersection(a, b, origin::taxiDistance);
     }
 
-    public static int findClosestIntersection(String a, String b) {
+    static int findClosestIntersection(String a, String b) {
         return findClosestIntersection(new Line(a), new Line(b));
     }
 
@@ -178,17 +175,17 @@ public class Day3 {
         return findIntersection(a, b, point -> a.delay(point) + b.delay(point));
     }
 
-    public static int findSmallestDelayIntersection(String a, String b) {
+    static int findSmallestDelayIntersection(String a, String b) {
         return findSmallestDelayIntersection(new Line(a), new Line(b));
     }
 
     public static void main(String[] args) {
         List<String> paths = FileUtils.readLines("day3/input.txt").collect(Collectors.toList());
 
-        int taxiDistance = findClosestIntersection( paths.get(0), paths.get(1));
+        int taxiDistance = findClosestIntersection(paths.get(0), paths.get(1));
         System.out.println("Manhattan (sic) distance: " + taxiDistance);
 
-        int smallestDelay = findSmallestDelayIntersection( paths.get(0), paths.get(1));
+        int smallestDelay = findSmallestDelayIntersection(paths.get(0), paths.get(1));
         System.out.println("Smallest delay: " + smallestDelay);
     }
 }
