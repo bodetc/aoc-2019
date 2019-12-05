d5042931a7eedfbe1af1d2bdc6fba2d9d17bb0cf
Cédric Bodet <cedric.bodet@futurice.com>
2019-12-05 12:55:44 +0200

    Day 5 - Intcode computer refactoring

--- src/main/java/intcode/IntcodeComputer.java
+++ src/main/java/intcode/IntcodeComputer.java
@@ -1,23 +1,45 @@
 package intcode;
 
 public class IntcodeComputer {
-    private final int[] program;
+    private static final int[] EMPTY_ARRAY = new int[]{};
 
-    public IntcodeComputer(int[] program) {
-        this.program = program;
+    private final int[] savedProgram;
+    private int[] program;
+
+    public IntcodeComputer(int[] savedProgram) {
+        this.savedProgram = savedProgram;
     }
 
-    static void intcode(int[] program) {
-        for (int pos = 0; pos < program.length; pos += 4) {
-            int opcode = program[pos];
+    public int runNounVerb(int noun, int verb) {
+        program = this.savedProgram.clone();
+        program[1] = noun;
+        program[2] = verb;
+
+        intcode(program);
+        return program[0];
+    }
+
+    int[] run() {
+        program = this.savedProgram.clone();
+        intcode(EMPTY_ARRAY);
+        return program;
+    }
+
+    private void intcode(int[] inputs) {
+        int inputIndex = 0;
+        int position = 0;
+
+        while (position < program.length) {
+            int instruction = program[position];
+            int opcode = getOptcode(instruction);
 
             if (opcode == 99) {
                 return;
             }
 
-            int pos1 = program[pos + 1];
-            int pos2 = program[pos + 2];
-            int posR = program[pos + 3];
+            int pos1 = program[position + 1];
+            int pos2 = program[position + 2];
+            int posR = program[position + 3];
 
             if (opcode == 1) {
                 program[posR] = program[pos1] + program[pos2];
@@ -26,16 +48,15 @@ public class IntcodeComputer {
             } else {
                 throw new IllegalStateException("Encountering an unknown opcode means something went wrong.");
             }
+
+            position+=4;
         }
-        throw new IllegalStateException("Intcode program should end when encountering 99.");
+
+        throw new IllegalStateException("Intcode savedProgram should end when encountering 99.");
     }
 
-    public int runNounVerb(int noun, int verb) {
-        int[] program = this.program.clone();
-        program[1]=noun;
-        program[2]=verb;
 
-        intcode(program);
-        return program[0];
+    private static int getOptcode(int instruction) {
+        return instruction % 100;
     }
 }
